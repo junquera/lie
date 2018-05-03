5c3db7e5cdcd870af98b31f927a50fcb3269c567
J. Junquera <javier.junquera.sanchez@gmail.com>
2018-05-03 11:49:56 +0200

    Updates

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
-browser.get('https://twitter.com')
+from getpass import getpass
+
+import time
+
+driver = webdriver.Firefox()
+driver.get('https://twitter.com')
 
 # https://twitter.com/{{user}}/likes
 def login_twitter(username, password):
-    driver = webdriver.Firefox()
+
     driver.get("https://twitter.com/login")
 
     username_field = driver.find_element_by_class_name("js-username-field")
@@ -23,5 +28,14 @@ def login_twitter(username, password):
 if __name__ == "__main__":
     username = input("user name : ")
     password = getpass("password  : ")
-    
+
 login_twitter(username, password)
+driver.get('https://twitter.com/eldiario/likes')
+html = driver.find_element_by_tag_name('html')
+
+for _ in range(10):
+    body = browser.find_element_by_tag_name('body')
+    time.sleep(2)
+
+body.send_keys(Keys.PAGE_DOWN)
+print(html)
